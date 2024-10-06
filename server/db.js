e709c1b9d1de690742d61167fa97a774fc3803ef
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-10-06 17:24:25 -0400

    build out db

--- server/db.js
+++ server/db.js
@@ -1,91 +1,89 @@
 const pg = require('pg');
+const express = require('express');
 const app = express();
 const port = 2000;
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_talent_agency_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_talent_agency_db'); ********************************
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 
 const createTables = async()=> {
   const SQL = `
-    DROP TABLE IF EXISTS users;
-    DROP TABLE IF EXISTS oauthusers;
-    DROP TABLE IF EXISTS items;
     DROP TABLE IF EXISTS reviews;
+    DROP TABLE IF EXISTS items;
+    DROP TABLE IF EXISTS oauthusers;
+    DROP TABLE IF EXISTS users;
+    
     
     CREATE TABLE users(
       id UUID PRIMARY KEY,
       username VARCHAR(100) UNIQUE NOT NULL,
       password VARCHAR(255)
+    );
 
-    CREATE TABLE oauthusers(                    ********************************
+    CREATE TABLE oauthusers(                                  ********************************
       github_id UUID PRIMARY KEY,
       login VARCHAR(100) UNIQUE NOT NULL,
       access_token VARCHAR(255)
-      
+    );
+
     CREATE TABLE items(
       id UUID PRIMARY KEY,
       name VARCHAR(100) UNIQUE NOT NULL,
-      details VARCHAR(100) UNIQUE NOT NULL,
-      rating VARCHAR(100) UNIQUE NOT NULL,
-      reviews_id UUID REFERENCES reviews(id) NOT NULL    ****************************
+      details VARCHAR(255),
+      rating VARCHAR(10),
+      reviews_id UUID REFERENCES reviews(id)
+    );
 
-    CREATE TABLE reviews(
+    CREATE TABLE reviews(                             ********************************              
       id UUID PRIMARY KEY,
       user_id UUID REFERENCES users(id) NOT NULL,
       item_id UUID REFERENCES items(id) NOT NULL,
-      rating VARCHAR(100) UNIQUE NOT NULL,
-      comments VARCHAR(100) UNIQUE NOT NULL,
+      rating VARCHAR(10),
+      comments VARCHAR(255),
       CONSTRAINT unique_item_user UNIQUE (item_id, user_id)
-);
+    );
   `;
   await client.query(SQL);
 };
 
-
+// create a user
 const createUser = async({ username, password })=> {
     const SQL = `
-      INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+      INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *;
     `;
     const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
     return response.rows[0];
   
   }
   
-  const createOauthUser = async({ username })=> {
+  // create an Oauth user
+  const createOauthUsers = async({ login })=> {
     const SQL = `
-      INSERT INTO users(id, username) VALUES($1, $2) RETURNING *
+      INSERT INTO oauthusers(github_id, login, access_token) VALUES($1, $2, $3) RETURNING *;
     `;
-    const response = await client.query(SQL, [uuid.v4(), username)]);
+    const response = await client.query(SQL, [uuid.v4(), login, uuid.v4()]);    //do I want to generate a token here?
     return response.rows[0];
   
   }
 
-
-
-
-
-
-
-
-
-
-
-const createUserSkill = async({ user_id, skill_id })=> {
+  // create an item
+  const createItem = async({ name, details, rating, reviews_id })=> {
     const SQL = `
-      INSERT INTO user_skills(id, user_id, skill_id) VALUES($1, $2, $3) RETURNING *
+      INSERT INTO items(id, name, details, rating, reviews_id) VALUES($1, $2, $3, $4, $5) RETURNING *;
     `;
-    const response = await client.query(SQL, [uuid.v4(), user_id, skill_id]);
+    const response = await client.query(SQL, [uuid.v4(), name, details, rating, reviews_id]);
+    return response.rows[0];
+  }
+  // create a review
+const createReview = async({ user_id, item_id, rating, comments })=> {
+    const SQL = `
+      INSERT INTO reviews(id, user_id, item_id, rating, comments) VALUES($1, $2, $3, $4, $5) RETURNING *;
+    `;
+    const response = await client.query(SQL, [uuid.v4(), user_id, item_id, rating, comments]);
     return response.rows[0];
   }
 
-  const createSkill = async({ name })=> {
-    const SQL = `
-      INSERT INTO skills(id, name) VALUES($1, $2) RETURNING *
-    `;
-    const response = await client.query(SQL, [uuid.v4(), name]);
-    return response.rows[0];
-  }
-   
+   // fetch all users
   const fetchUsers = async()=> {
     const SQL = `
       SELECT * FROM users;
@@ -94,26 +92,39 @@ const createUserSkill = async({ user_id, skill_id })=> {
     return response.rows;
   }
   
-  const fetchSkills = async()=> {
+  // fetch all items
+  const fetchItems = async()=> {
     const SQL = `
-      SELECT * FROM skills;
+      SELECT * FROM items;
     `;
     const response = await client.query(SQL);
     return response.rows;
   }
 
-  const fetchUserSkills = async(id)=> {
+  //fetch review from a specific user
+  const fetchUserReviews = async(id)=> {
     const SQL = `
-      SELECT * FROM user_skills
+      SELECT * FROM reviews
       WHERE user_id = $1
     `;
     const response = await client.query(SQL, [ id ]);
     return response.rows;
   }
   
-  const deleteUserSkill = async({id, user_id})=> {
+//fetch review from a specific item
+const fetchItemReviews = async(id)=> {
+  const SQL = `
+    SELECT * FROM reviews
+    WHERE item_id = $1
+  `;
+  const response = await client.query(SQL, [ id ]);
+  return response.rows;
+}
+
+// delete a review
+  const deleteReview = async({id, user_id})=> {
     const SQL = `
-      DELETE FROM user_skills
+      DELETE FROM reviews
       WHERE id = $1 AND user_id = $2
     `;
     await client.query(SQL, [ id, user_id ]);
@@ -125,10 +136,13 @@ const createUserSkill = async({ user_id, skill_id })=> {
     client,
     createTables,
     createUser,
-    createSkill,
+    createOauthUsers,
+    createItem,
     fetchUsers,
-    fetchSkills,
-    fetchUserSkills,
-    createUserSkill,
-    deleteUserSkill
-  }; */
+    fetchOauthUsers
+    fetchItems,
+    fetchUserReviews,
+    fetchItemReviews,
+    createReview,
+    deleteReview
+  };
