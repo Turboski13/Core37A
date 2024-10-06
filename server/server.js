50f1ea0967d8da5abaf3c0362ebef00887e8175e
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-10-06 17:35:54 -0400

    build out server js

--- server/server.js
+++ server/server.js
@@ -3,118 +3,21 @@ const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
 const PORT = 8080;
-
-
-
-/* const {
+const {
     client,
     createTables,
     createUser,
-    createSkill,
-    createUserSkill,
+    createOauthUsers,
+    createItem,
     fetchUsers,
-    fetchSkills,
-    fetchUserSkills,
-    userUserSkill,
-    deleteUserSkill
+    fetchOauthUsers
+    fetchItems,
+    fetchUserReviews,
+    fetchItemReviews,
+    createReview,
+    deleteReview
   } = require('./db');
 const app = express();
-
-app.get('/api/users', async(req, res, next)=> {
-    try {
-      res.send(await fetchUsers());
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-  
-app.get('/api/skills', async(req, res, next)=> {
-    try {
-      res.send(await fetchSkills());
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-
-app.get('/api/users/:id/userSkills', async(req, res, next)=> {
-    try {
-      res.send(await fetchUserSkills(req.params.id));
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-
-app.post('/api/users/:id/userSkills', async(req, res, next)=> {
-    try {
-      res.status(201).send(await createUserSkill({ user_id: req.params.id, skill_id: req.body.skill_id}));
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-  
-app.delete('/api/users/:userId/userSkills/:id', async(req, res, next)=> {
-    try {
-      await deleteUserSkill({ id: req.params.id, user_id: req.params.userId });
-      res.sendStatus(204);
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-
-  const init = async()=> {
-    await client.connect();
-    console.log('connected to database');
-    await createTables();
-    console.log('tables created');
-    const [moe, lucy, ethyl, singing, dancing, juggling, plateSpinning] = await Promise.all([
-      createUser({ username: 'moe', password: 's3cr3t' }),
-      createUser({ username: 'lucy', password: 's3cr3t!!' }),
-      createUser({ username: 'ethyl', password: 'shhh' }),
-      createSkill({ name: 'singing'}),
-      createSkill({ name: 'dancing'}),
-      createSkill({ name: 'juggling'}),
-      createSkill({ name: 'plate spinning'}),
-    ]);
-    const users = await fetchUsers();
-    console.log(users);
-  
-    const skills = await fetchSkills();
-    console.log(skills);
-  
-    const userSkills = await Promise.all([
-        createUserSkill({ user_id: moe.id, skill_id: plateSpinning.id}),
-        createUserSkill({ user_id: moe.id, skill_id: juggling.id}),
-        createUserSkill({ user_id: ethyl.id, skill_id: juggling.id}),
-        createUserSkill({ user_id: lucy.id, skill_id: dancing.id}),
-      ]);
-    
-      console.log(await fetchUserSkills(moe.id));
-      await deleteUserSkill({ user_id: moe.id, id: userSkills[0].id});
-      console.log(await fetchUserSkills(moe.id));
-    
-      console.log(`curl localhost:3000/api/users/${ethyl.id}/userSkills`);
-    
-      console.log(`curl -X POST localhost:3000/api/users/${ethyl.id}/userSkills -d '{"skill_id": "${dancing.id}"}' -H 'Content-Type:application/json'`);
-      console.log(`curl -X DELETE localhost:3000/api/users/${ethyl.id}/userSkills/${userSkills[3].id}`);
-      
-      console.log('data seeded');
-    
-      const port = process.env.PORT || 3000;
-      app.listen(port, ()=> console.log(`listening on port ${port}`));
-    };
-    
-    init();
-
-  */
- 
- 
- 
-
 
 app.use(cors({
   origin: 'http://localhost:5173', // Allow requests from React app
@@ -171,8 +74,6 @@ app.get('/auth/github/callback', async (req, res) => {
       res.status(500).json({ error: 'Failed to authenticate with GitHub' });
   }
 });
-  
-  
    
   
   // API route to get the user data from PostgreSQL using GitHub ID
@@ -204,3 +105,133 @@ app.get('/auth/github/callback', async (req, res) => {
   app.listen(PORT, () => {
       console.log(`Backend server is running on http://localhost:${PORT}`);
   });
+
+
+
+
+// now to build out the app.gets
+
+
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send(await fetchUsers());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+
+app.get('/api/items', async(req, res, next)=> {
+    try {
+      res.send(await fetchItems());
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+
+app.get('/api/users/:id/reviews', async(req, res, next)=> {
+    try {
+      res.send(await fetchUserReviews(req.params.id));
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+app.get('/api/item/:id/reviews', async(req, res, next)=> {
+    try {
+      res.send(await fetchItemReviews(req.params.id));
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+
+// now to build out the app.posts
+
+app.post('/api/users/:id/reviews', async(req, res, next)=> {
+  try {
+    res.status(201).send(await createReview({ user_id: req.params.id, item_id: req.body.item_id}));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+// now to build out the app.deletes
+
+app.delete('/api/users/:userId/reviews/:id', async(req, res, next)=> {
+    try {
+      await deleteReview({ id: req.params.id, user_id: req.params.userId });
+      res.sendStatus(204);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+  app.delete('/api/item/:itemId/reviews/:id', async(req, res, next)=> {
+    try {
+      await deleteReview({ id: req.params.id, item_id: req.params.userId });
+      res.sendStatus(204);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+
+
+  const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    console.log('tables created');
+    /* const [moe, lucy, ethyl, singing, dancing, juggling, plateSpinning] = await Promise.all([
+      createUser({ username: 'moe', password: 's3cr3t' }),
+      createUser({ username: 'lucy', password: 's3cr3t!!' }),
+      createUser({ username: 'ethyl', password: 'shhh' }),
+      createSkill({ name: 'singing'}),
+      createSkill({ name: 'dancing'}),
+      createSkill({ name: 'juggling'}),
+      createSkill({ name: 'plate spinning'}),
+    ]); */
+    const users = await fetchUsers();
+    console.log(users);
+  
+    const skills = await fetchItems();
+    console.log(skills);
+  
+    /* const userSkills = await Promise.all([
+        createUserSkill({ user_id: moe.id, skill_id: plateSpinning.id}),
+        createUserSkill({ user_id: moe.id, skill_id: juggling.id}),
+        createUserSkill({ user_id: ethyl.id, skill_id: juggling.id}),
+        createUserSkill({ user_id: lucy.id, skill_id: dancing.id}),
+      ]); */
+    
+      c/* onsole.log(await fetchUserReviews(X.id));
+      await deleteReview({ user_id: X.id, id: reviews[0].id});
+      console.log(await fetchUserReviews(moe.id));
+    
+      console.log(`curl localhost:3000/api/users/${XX.id}/reviews`);
+    
+      console.log(`curl -X POST localhost:3000/api/users/${XX.id}/reviews -d '{"skill_id": "${Y.id}"}' -H 'Content-Type:application/json'`);
+      console.log(`curl -X DELETE localhost:3000/api/users/${XX.id}/reviews/${Y[3].id}`); */
+      
+      console.log('data seeded');
+    
+      const port = process.env.PORT || 3000;
+      app.listen(port, ()=> console.log(`listening on port ${port}`));
+    };
+    
+    init();
+
+ 
+ 
+ 
+
+
